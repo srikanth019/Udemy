781a7bf749fffec2103e1869a69dd3aa976c4be6
srikanth019 <122976606+srikanth019@users.noreply.github.com>
2023-01-18 17:42:14 +0530

    Delete asgmt1 directory

--- asgmt1/app.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const {requestHandler, userText} = require("./rout");
-console.log(userText)
-const server = http.createServer(requestHandle);
-//console
-server.listen(3000)
